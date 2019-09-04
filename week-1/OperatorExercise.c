5b5841674c50c49a032e991ffa4d0c8fdc7afd69
yuhanyuhan <yuhanlian@gmail.com>
2019-09-04 18:07:16 +0800

    Refactored Final Work

--- week-1/OperatorExercise.c
+++ week-1/OperatorExercise.c
@@ -19,15 +19,15 @@ int main(void)
   printf("Multiply A with C : %d\n", A * C);
   printf("Find the whole number of C divide by A : %d\n", C / A);
   printf("Find the remainder of C divide by A : %d\n", C % A);
-  printf("Add A and B, multiply that result with C : %d\n", (A+B)*C);
+  printf("Add A and B, multiply that result with C : %d\n", (A + B)*C);
   printf("Add 1 to A (This will update A) : %d\n", ++A);
-  printf("Multiply B to C (This will update C) : %d\n", C*=B);
-  printf("Add B and C to A (This will update A) then subtract 1 from B and C : %d\n\n", A += ((B + C)-1));
+  printf("Multiply B to C (This will update C) : %d\n", C *= B);
+  printf("Add B and C to A (This will update A) then subtract 1 from B and C : %d\n\n", A += ((B + C) -1));
 
   /* Section B : Fill in the output with the correct single line code statement matching the text (Relational and Logical) */
   printf("// Section B -------------------------------------\n");
   printf("Given numbers A = %d, B = %d, C = %d\n", A, B, C);
-  printf("Is A not zero? (TRUE/FALSE): %s\n", A!=0 ? "TRUE" : "FALSE");
+  printf("Is A not zero? (TRUE/FALSE): %s\n", A != 0 ? "TRUE" : "FALSE");
   printf("Is A greater than C? (TRUE/FALSE): %s\n", A > C ? "TRUE" : "FALSE");
   printf("Is B lesser than A and C? (TRUE/FALSE) : %s\n", B < A && B < C ? "TRUE" : "FALSE");
   printf("Is C an odd number? (TRUE/FALSE): %s\n", C % 2 == 1 ? "TRUE" : "FALSE");
@@ -41,7 +41,7 @@ int main(void)
   printf("Given numbers A = %d, B = %d, C = %d\n", A, B, C);
   printf("Flip all the bits of B : %d\n", ~B);
   /* since it is placed with %d, answer is assumed to be a decimal. */
-  printf("Find the common bits of A and C : %d\n", ~(A ^ C ));
+  printf("Find the common bits of A and C : %d\n", ~(A ^ C));
   printf("Find the different bits of A and C : %d\n", A ^ C);
   printf("Shift all the bits in A to the left by 1 : %d\n", A<<1);
   printf("Is the 4th rightmost bit of A a 1? (TRUE/FALSE): %s\n", ((A>>3) & 1) ? "TRUE":"FALSE");
