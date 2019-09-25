aef1b9af45889c9cd4bdc96bc96af666ae83b7fe
yuhanyuhan <yuhanlian@gmail.com>
2019-09-25 09:21:37 +0800

    update class activty

--- week-3/ptr.c
+++ week-3/ptr.c
@@ -1,10 +1,41 @@
 #include <stdio.h> 
 
-
-int main () {
-    int x = 255;
-    x += 1;
-
-    printf("%d", x);
+int main()
+{
+    int *ptr;
+    int x;
+ 
+    ptr = &x;
+    *ptr = 0;
+ 
+    printf(" x = %dn", x);
+    printf(" ptr = %dn", ptr);
+    printf(" *ptr = %dn", *ptr);
+ 
+    *ptr += 5;
+    printf(" x  = %dn", x);
+    printf(" *ptr = %dn", *ptr);
+ 
+    (*ptr)++;
+    printf(" x = %dn", x);
+    printf(" *ptr = %dn", *ptr);
+ 
+    return 0;
 }
+
+// simple pointer and de-referencing
+// void fun (int* p){
+//     int q = 10; 
+//     p = &q; 
+//     printf("%d\n", *p); 
+// }
+
+// int main () {
+//     int r = 20; 
+//     int* p = &r; 
+//     fun(p); 
+//     printf("%d\n", *p); 
+//     printf("end of line"); 
+//     return 0; 
+// }
 
---
+++ week-4/struct.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  
+
+struct Student {
+    char* Name; 
+    float GPA; 
+    int Age; 
+}; 
+
+int main() {
+    
+}
