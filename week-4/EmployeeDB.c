81b093b4e5d691f702b327651f0890566a11b02c
yuhanyuhan <yuhanlian@gmail.com>
2019-09-26 22:01:50 +0800

    update employee db assignment

--- week-4/EmployeeDB.c
+++ week-4/EmployeeDB.c
@@ -6,127 +6,116 @@ struct Employee
 {
     int ID; // autoincrement
     char *Name;
-    int Salary;  //check for zero
+    int Salary;      //check for zero
     int phoneNumber; //optional
-    struct Employee* next; 
+    struct Employee *next;
 };
 
+struct Employee *CreateNode(int ID, const char *Name, int Salary, int phoneNumber)
+{
+    struct Employee *newNode = NULL;
+
+    int nameLength = strlen(Name); /* Get the length of our string */
+    char *newString = malloc(sizeof(char) * nameLength + 1);
+    if (newString == NULL)
+        return NULL; /* Failed to allocate our string, can't proceed */
+
+    newNode = malloc(sizeof(struct Employee));
+    if (newNode == NULL)
+    {
+        free(newString); /* remember that we allocated but now can't use */
+        return NULL;     /* This case means cannot allocate */
+    }
+
+    strcpy(newString, Name); /* Copy the string value over */
+
+    newNode->next = NULL; /* Ensure next is NULL */
+                          /* Time to copy our value to our node */
+    newNode->ID = ID;
+    newNode->Name = newString;
+    newNode->Salary = Salary;
+    newNode->phoneNumber = phoneNumber;
+
+    return newNode;
+}
 
 // add new employee node at the end
-void AddToEnd(struct Node** Employee, const char* value)
+void AddToEnd(struct Employee **ppEmployee, int ID, const char *Name, int Salary, int phoneNumber)
 {
-	struct Node* curr = *Employee;
-	
-	struct Node* newNode = CreateNode(value);
-	if (newNode == NULL)
+    struct Employee *curr = *ppEmployee;
+
+    struct Employee *newNode = CreateNode(ID, Name, Salary, phoneNumber);
+    if (newNode == NULL)
     {
         printf("AddToEnd failed (CreateNode failed)\n");
         return; /* Cannot allocate the node */
     }
-		
-	
-	if (curr == NULL)
-	{
-		/* We don't even have a single node */
-		/* Time to create and set it as the first */		
-		/* Malloc is successful, time to link to the first */
-		*ppList = newNode;
-		return;
-	}
-	
-	/* Means that we are on a node, need to ensure that this is the LAST NODE */
-	while(curr->next != NULL)
-	{
-		curr = curr->next;
-	}
-	
-	/* This means we are at the LAST NODE! */
-	curr->next = newNode;
-}
 
-
-
-// remove employee node by ID
-void deleteNode(struct Node **head_ref, int key) 
-{ 
-    // Store head node 
-    struct Node* temp = *head_ref, *prev; 
-  
-    // If head node itself holds the key to be deleted 
-    if (temp != NULL && temp->data == key) 
-    { 
-        *head_ref = temp->next;   // Changed head 
-        free(temp);               // free old head 
-        return; 
-    } 
-  
-    // Search for the key to be deleted, keep track of the 
-    // previous node as we need to change 'prev->next' 
-    while (temp != NULL && temp->data != key) 
-    { 
-        prev = temp; 
-        temp = temp->next; 
-    } 
-  
-    // If key was not present in linked list 
-    if (temp == NULL) return; 
-  
-    // Unlink the node from linked list 
-    prev->next = temp->next; 
-  
-    free(temp);  // Free memory 
-} 
-
-
-// printall
-void PrintList(struct Node* list)
-{
-	int i = 0;
-	while (list != NULL)
-	{
-		/* We are on an actual node */
-		printf("Node[%d]: %s\n", i++, list->value);
-		list = list->next;
-	}
-}
-
-// search
-bool search(struct Node* head, int x) 
-{ 
-    struct Node* current = head;  // Initialize current 
-    while (current != NULL) 
-    { 
-        if (current->key == x) 
-            return true; 
-        current = current->next; 
-    } 
-    return false; 
-
-// quit and remove all entries (free memory)
-void FreeList(struct Node** pList)
-{
-	struct Node* curr = *pList;
-    struct Node* temp = 0;
-    if(curr == NULL)
+    if (curr == NULL)
     {
-        printf("FreeList failed (Linked List is empty)");
+        /* We don't even have a single node */
+        /* Time to create and set it as the first */
+        /* Malloc is successful, time to link to the first */
+        *ppEmployee = newNode;
         return;
     }
-    while(curr != NULL)
-    {       
-        temp = curr->next;
-        free(curr->value);
-        free(curr);
-        curr = temp;
+
+    /* Means that we are on a node, need to ensure that this is the LAST NODE */
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
     }
-    *pList = NULL;
+
+    /* This means we are at the LAST NODE! */
+    curr->next = newNode;
 }
 
-// handle Cases 
+void PrintList(struct Employee *list)
+{
+    int i = 0;
+    while (list != NULL)
+    {
+        /* We are on an actual node */
+        printf("Node[%d]: %d\n", i++, list->ID);
+        list = list->next;
+    }
+}
+
+int Count(struct Employee *list)
+{
+    int count = 0;
+    while (list != NULL)
+    {
+        /* We are on an actual node */
+        ++count;
+        list = list->next;
+    }
+    return count;
+}
+
+int isANum(char *input)
+{
+    int count = 0;
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        if (input[i] >= '0' && input[i] <= '9')
+        {
+            count++;
+        }
+    }
+    int result = count == strlen(input) ? 1 : 0;
+
+    return result;
+}
+
 int main()
 {
+
     printf("Welcome to our Employee Database!\n");
     char input[100];
+    int latestID = 0;
+
+    struct Employee *employeeHead = NULL;
 
     do
     {
@@ -135,40 +124,39 @@ int main()
         strtok(input, " ");
         if (strcmp(input, "add") == 0)
         {
-            struct Employee employee1;
 
             char *Name = strtok(NULL, " ");
             char *Salary = strtok(NULL, " ");
             char *phoneNumber = strtok(NULL, " ");
-            
-            employee1.Name = Name;
-            employee1.Salary = atoi(Salary);
-            if (phoneNumber != NULL) {
-                employee1.phoneNumber = atoi(phoneNumber);
-            } else {
-                employee1.phoneNumber = 0; 
+
+            int aSalary;
+            int aPhoneNumber;
+
+            // reject non-number 
+            if (Salary == NULL || isANum(Salary) == 0)
+            {
+                printf("Invalid input, please try again.\n");
+                continue;
+            }
+            else
+            {
+                aSalary = atoi(Salary);
             }
 
-            if (employee1.Salary == 0) {
-                printf("Invalid input, please try again."); 
-                continue; 
-            } 
+            if (phoneNumber != NULL)
+            {
+                aPhoneNumber = atoi(phoneNumber);
+            }
+            else
+            {
+                aPhoneNumber = 0;
+            }
 
-            printf("Name: %s\n", employee1.Name);
-            printf("Salary: %d\n", employee1.Salary);
-            printf("Phone Number: %d\n", employee1 .phoneNumber);
+            AddToEnd(&employeeHead, latestID, Name, aSalary, aPhoneNumber);
+    
+            printf("SUCCESS\n"); 
         }
-
         if (strcmp(input, "remove") == 0)
             printf("%s\n", input);
-        if (strcmp(input, "printall") == 0)
-            printf("%s\n", input);
-        if (strcmp(input, "search") == 0)
-            printf("%s\n", input);
-        if (strcmp(input, "quit") == 0) 
-            printf("bye bye!"); 
-
     } while (strcmp(input, "quit") != 0);
-
-    return 0;
 }
