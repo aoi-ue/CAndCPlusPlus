18a2aeda6d6df8d5481b39f7f4725f615f87a675
yuhanyuhan <yuhanlian@gmail.com>
2019-10-17 14:52:02 +0800

    submission of AVL tree

--- week-6/BinarySearchTree.cpp
+++ week-6/BinarySearchTree.cpp
@@ -4,70 +4,88 @@ using namespace std;
 
 struct Node
 {
-	int value;
-	struct Node *left, *right;
-	int height;
+    int value;
+    struct Node *left, *right;
+    int height;
 };
-
-Node *newNode(int item)
-{
-	Node *temp = new Node;
-	temp->value = item;
-	temp->left = temp->right = NULL;
-	return temp;
-}
 
 int height(Node *node)
 {
-	if (node == NULL)
-		return -1;
-	int leftH = height(node->left);
-	int rightH = height(node->right);
+    if (node == NULL)
+        return -1;
+    int leftH = height(node->left);
+    int rightH = height(node->right);
 
-	if (leftH > rightH)
-	{
-		return leftH + 1;
-	}
-	else
-	{
-		return rightH + 1;
-	}
+    if (leftH > rightH)
+    {
+        return leftH + 1;
+    }
+    else
+    {
+        return rightH + 1;
+    }
 }
 
-void rotateRight(Node **root)
+Node *newNode(int value)
+{
+    Node *temp = new Node;
+    temp->value = value;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+Node *rotateRight(Node **root)
 {
     Node *temp = *root;
     *root = (*root)->left;
     temp->left = (*root)->right;
     (*root)->right = temp;
+    return *root; 
 }
 
-void rotateLeft(Node **root)
+Node *rotateLeft(Node **root)
 {
     Node *temp = *root;
     *root = (*root)->right;
     temp->right = (*root)->left;
     (*root)->left = temp;
-}
+    return *root; 
 
-int balanceFactor(Node *node)
-{
-    if (node == NULL) return 0;
-    return height(node->left) - height(node->right);
 }
 
 void inorder(Node *root)
 {
-	if (root != NULL)
-	{
-		inorder(root->left);
-		printf("%d ", root->value);
-		inorder(root->right);
-	}
+    if (root != NULL)
+    {
+        inorder(root->left);
+        printf("%d ", root->value);
+        inorder(root->right);
+    }
 }
 
+int balanceFactor(Node *node)
+{
+    if (node == NULL)
+        return 0;
+    return height(node->left) - height(node->right);
+}
 
-/* Practice Two - AVL Tree insertion */
+Node *search(Node *node, int value) 
+{
+
+    if (node == NULL) return NULL; 
+    if (node->value == value) return node; 
+
+    if (node->value < value) 
+    {
+        return search(node->right, value); 
+    } 
+    else 
+    {
+        return search(node->left, value); 
+    }
+}
+
 Node *insert(Node *node, int value)
 {
     if (node == NULL)
@@ -77,70 +95,69 @@ Node *insert(Node *node, int value)
     {
         node->left = insert(node->left, value);
     }
-    else {
+    else
+    {
         node->right = insert(node->right, value);
     }
 
-    int balance = balanceFactor(node); 
+    int balance = balanceFactor(node);
 
-    // Left Left Case  
-    if (balance > 1 && value < node->left->value)  
-        rotateRight(&(node));  
-  
-    // Right Right Case  
-    if (balance < -1 && value > node->right->value)  
-        rotateLeft(&(node));  
-  
-    // Left Right Case  
-    if (balance > 1 && value > node->left->value)  
-    {  
-        rotateLeft(&node->left);  
-        rotateRight(&(node));  
-    }  
-    // Right Left Case  
-    if (balance < -1 && value < node->right->value)  
-    {  
-        rotateRight(&node->right);  
-        rotateLeft(&(node));  
-    }  
+    // Left Left Case
+    if (balance > 1 && value < node->left->value)
+        return rotateRight(&(node));
+
+    // Right Right Case
+    if (balance < -1 && value > node->right->value)
+        return rotateLeft(&(node));
+
+    // Left Right Case
+    if (balance > 1 && value > node->left->value)
+    {
+        node->left = rotateLeft(&(node)->left);
+        return rotateRight(&(node));
+    }
+    // Right Left Case
+    if (balance < -1 && value < node->right->value)
+    {
+        node->left = rotateRight(&(node)->right);
+        return rotateLeft(&(node));
+    }
     return node;
 }
 
-/* Practice One - AVL Tree Delete */
-Node *deleteNode(Node *root, int k)
+Node *deleteNode(Node *node, int value)
 {
-	if (root == NULL)
-		return root;
+    if (node == NULL)
+        return node;
 
-	if (root->value > k)
-	{
-		root->left = deleteNode(root->left, k);
-		return root;
-	}
-	else if (root->value < k)
-	{
-		root->right = deleteNode(root->right, k);
-		return root;
-	}
-
-	if (root->left = = NULL)
-	{
-		Node *temp = root->right;
-		delete root;
-		return temp;
-	}
-	else if (root->right == NULL)
-	{
-		Node *temp = root->left;
-		delete root;
-		return temp;
-	}
-
+    if (node->value > value)
+    {
+        node->left = deleteNode(node->left, value);
+        return node;
+    }
+    else if (node->value < value)
+    {
+        node->right = deleteNode(node->right, value);
+        return node;
+    }
+    // delete right selected node
+    if (node->left == NULL)
+    {
+        Node *temp = node->right;
+        delete node;
+        return temp;
+    }
+    // delete left selected node
+    else if (node->right == NULL)
+    {
+        Node *temp = node;
+        delete node;
+        return temp;
+    }
 	else
 	{
-
-		Node *succParent = root;
-		Node *succ = root->right;
+		Node *succParent = node;
+		Node *succ = node->right;
 
 		while (succ->left != NULL)
 		{
@@ -148,19 +165,18 @@ Node *deleteNode(Node *root, int k)
 			succ = succ->left;
 		}
 
-
-		root->value = succ->value;
+		node->value = succ->value;
 
 		delete succ;
 		succParent->left = NULL; 
-		return root;
+		return node;
 	}
 }
 
 int main()
 {
 
-	Node *root = NULL;
+    Node *root = NULL;
 	root = insert(root, 50);
 	root = insert(root, 30);
 	root = insert(root, 20);
@@ -169,27 +185,26 @@ int main()
 	root = insert(root, 60);
 	root = insert(root, 80);
 
-	// printf("Rotate to Right\n");
-	// rotateRight(root);
-	// cout << "height of tree " << height(root) << endl;
+    // Node *item = search(root, 40); 
+    // (item == NULL) ? cout << "Not Found" : cout << "Search found: " << item->value << endl; 
 
-	printf("Inorder traversal of the given tree \n");
-	inorder(root);
+    cout << "Inorder traversal of the AVL tree" << endl; 
+    inorder(root);
+    cout << "Height of Tree is at " << height(root) << endl;
 
-	printf("\nDelete 20\n");
-	root = deleteNode(root, 20);
-	printf("Inorder traversal of the modified tree \n");
-	inorder(root);
+    cout << "Inorder traversal of the delete one node AVL tree" << endl; 
+    root = deleteNode(root, 50);
+    inorder(root);
+    cout << "Height of Tree is at " << height(root) << endl;
 
-	printf("\nDelete 30\n");
-	root = deleteNode(root, 30);
-	printf("Inorder traversal of the modified tree \n");
-	inorder(root);
+    cout << "Inorder traversal of the delete two node AVL tree" << endl; 
+    root = deleteNode(root, 70);
+    inorder(root);
+    cout << "Height of Tree is at " << height(root) << endl;
 
-	printf("\nDelete 50\n");
-	root = deleteNode(root, 50);
-	printf("Inorder traversal of the modified tree \n");
-	inorder(root);
+    cout << "Inorder traversal of the delete three node AVL tree" << endl; 
+    root = deleteNode(root, 40);
+    inorder(root);
+    cout << "Height of Tree is at " << height(root) << endl;
 
-	return 0;
 }
