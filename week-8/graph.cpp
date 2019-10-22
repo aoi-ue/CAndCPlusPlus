dad0e1d4ba7077822bbbeb69edf2c9962cac5f84
yuhanyuhan <yuhanlian@gmail.com>
2019-10-22 12:54:51 +0800

    include DS-graph

---
+++ week-8/graph.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <list> 
+
+using namespace std; 
+
+// Node in Graph
+// Graph Structure
+// Breadth First Search
+class Graph
+{
+    public:
+        void BFS(); 
+
+    private: 
+        void createNode() 
+        {
+            list<char> mylist (2,100);         // two ints with a value of 100
+        }
+};
+
+// Make Graph
+int main()
+{
+    Graph graph1;
+}
